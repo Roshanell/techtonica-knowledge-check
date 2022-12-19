bca4a33f9a9a092f9b8c18ba3fb8a71bf93b3643
Roshanell Francisco <roshanellf@gmail.com>
2022-12-19 08:36:01 -0600

    changed file types to js

---
+++ Working and Collaborating Remotely.js
@@ -0,0 +1,10 @@
+// // Check for Understanding
+// // Go onto Youtube or search for blog posts about the “pros and cons” of remote work, the etiquette of communicating online, or professional growth
+//  for remote workers. Write a short summary of what you found to be a surprise or misconception, or on the other hand, solidified your beliefs about
+//  the subject.
+
+// // Something I did not realize was that we should get ready as if we awere going to go into the office and set a regualar work schedule.
+// Something that I thought was clever was to overestimate your time so you can exceed on deliverables. Also, being able to schedule out your day is really
+// important. Setting boundries is one that I feel will be really imperative. Letting others know how and when to approach when working. Perhaps seting a sign
+// or letting them know our schedule work breaks. Lastly, building relationships is important even though you are remote. Setting time up outside of the regular
+// 9-5 to socialize can help strengthen those bonds.-->
