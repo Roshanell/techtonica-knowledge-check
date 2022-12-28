969ddba2a4b6c17dd625be587f188383216b459a
Roshanell Francisco <roshanellf@gmail.com>
2022-12-27 22:32:16 -0600

    knowledge check complete

--- Objects from the Odin Project.js
+++ Objects from the Odin Project.js
@@ -24,3 +24,41 @@ const atomicHabits = new makeBook(
 );
 console.log(theHobbit.info());
 console.log(atomicHabits.info());
+
+// Knowledge Check 1
+
+function character(name, age, skill) {
+	this.name = name;
+	this.age = age;
+	this.skill = skill;
+	this.information = function () {
+		return `${name} is ${age} years old and the is a ${skill}.`;
+	};
+}
+
+const tj = new character("Tyron Brooks Jr", 4, "brainiac");
+console.log(tj.information());
+
+// Knowledge Check 2
+
+// a prototype is an object that is able to access all of another's objects  methods and properties.
+
+// Knowledge Check 3
+
+// prototypical inheritence is a feauture in JS that is only accessible to objet and null datatypes. This allows developers
+// to set an object to a value in one piece of code and easily be able to reference, reuse, and extend that object's property in another piece of
+// code. As a result you do not have to rewrite code and it makes your code dry.
+
+// Knowledge Check 4
+
+// Do
+// Use Object.Create to set a prototype of an object
+
+// Don't
+// Dont use write/delete operations unless you want work directly sith the object.
+
+// Knowledge Check 5
+
+// Object.Create allows you to create a new object and uses an existing object as the prototype of the new object
+
+// Knowledge Check 6
